e73b9640cff31bcfbce2018f2cb4db2a48cbff3e
Jobayer_ak <jobayer.ak@gmail.com>
2023-05-02 23:42:19 +0600

    practiced spread, rest and default parameteres and destructuing

--- dist/function.js
+++ dist/function.js
@@ -1,6 +1,38 @@
 "use strict";
 // normal function
-function add(num1, num2) {
+// default parameer
+function add(num1, num2 = 10) {
     return num1 + num2;
 }
-add(1, 2);
+add(30, 50);
+// arrow function
+const addArrow = (num1, num2) => num1 + num2;
+const arr = [1, 3, 5];
+const newArray = arr.map((element) => element * element);
+const person1 = {
+    name: 'json',
+    balance: 5,
+    addBalance(money) {
+        console.log(`My balance is ${this.balance + money}`);
+    },
+};
+// spread operator
+const myFriends = ['chandler', 'johny', 'ross'];
+const newFriends = ['jessy', 'rachel', 'sirry'];
+myFriends.push(...newFriends);
+// console.log('My friends: ', myFriends);
+const myBestFriend = {
+    fullName: 'Addy Ron',
+    age: 24,
+};
+const [bestFreind] = myFriends;
+const { fullName } = myBestFriend; // when destructuring, don't need to mention type because typescript infers type
+console.log('Full Name: ', fullName);
+// rest perameter
+const greetFriends = (...friends) => {
+    // console.log(`Hi ${friend1}\n Hi ${friend2}\n Hi ${friend3}`);
+    // console.log('Freinds: ', friends);
+    friends.forEach((friend) => console.log('Friend: ', friend));
+};
+greetFriends('john', 'jason', 'Henry', 'Josh', 'Derran');
+// array and object destructuring
--- src/function.ts
+++ src/function.ts
@@ -1,7 +1,10 @@
 // normal function
-function add(num1: number, num2: number): number {
+// default parameer
+function add(num1: number, num2: number = 10): number {
   return num1 + num2;
 }
+
+add(30, 50);
 
 // arrow function
 const addArrow = (num1: number, num2: number): number => num1 + num2;
@@ -9,3 +12,41 @@ const addArrow = (num1: number, num2: number): number => num1 + num2;
 const arr = [1, 3, 5];
 
 const newArray = arr.map((element: number) => element * element);
+
+const person1: {
+  name: string;
+  balance: number;
+  addBalance(money: number): void;
+} = {
+  name: 'json',
+  balance: 5,
+  addBalance(money: number) {
+    console.log(`My balance is ${this.balance + money}`);
+  },
+};
+
+// spread operator
+const myFriends = ['chandler', 'johny', 'ross'];
+const newFriends = ['jessy', 'rachel', 'sirry'];
+
+myFriends.push(...newFriends);
+
+// console.log('My friends: ', myFriends);
+const myBestFriend = {
+  fullName: 'Addy Ron',
+  age: 24,
+};
+const [bestFreind] = myFriends;
+const { fullName } = myBestFriend; // when destructuring, don't need to mention type because typescript infers type
+console.log('Full Name: ', fullName);
+
+// rest perameter
+const greetFriends = (...friends: string[]): void => {
+  // console.log(`Hi ${friend1}\n Hi ${friend2}\n Hi ${friend3}`);
+  // console.log('Freinds: ', friends);
+  friends.forEach((friend) => console.log('Friend: ', friend));
+};
+
+greetFriends('john', 'jason', 'Henry', 'Josh', 'Derran');
+
+// array and object destructuring
